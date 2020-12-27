76475046cd38b238e60d7edf8667dd01f6c2e5b2
Mike Owens <mocoding86@gmail.com>
2020-12-27 10:14:38 -0800

    learn about setting defaults in axios for setting global configurtaion

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios';
 
+axios.defaults.baseURL = `https://jsonplaceholder.typicode.com`;
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
 axios.interceptors.request.use(request => {
     console.log(request);
     // Edit request config
@@ -21,7 +25,12 @@ axios.interceptors.response.use(response => {
  }, error => {
         console.log(error);
         return Promise.reject(error);
-    });
+});
+
+// You learned how to add an interceptor, getting rid of one is also easy. Simply store the reference to the interceptor in a variable and call eject  with that reference as an argument, to remove it (more info: https://github.com/axios/axios#interceptors):
+
+// var myInterceptor = axios.interceptors.request.use(function () {/*...*/});
+// axios.interceptors.request.eject(myInterceptor);
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
