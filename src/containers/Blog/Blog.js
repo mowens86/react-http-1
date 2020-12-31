402b575a0cac5e7c8e7d757a4a7347c3eaf1e053
Mike Owens <mocoding86@gmail.com>
2020-12-30 16:31:42 -0800

    work with nested routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 import Posts from './Posts/Posts';
 import { Route, Switch, NavLink } from 'react-router-dom';
 import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
 
 import './Blog.css';
 
@@ -16,7 +15,7 @@ class Blog extends Component {
                     <nav>
                         <ul>
                             <li><NavLink 
-                            to="/"
+                            to="/posts/"
                             exact
                             activeClassName="my-active"
                             activeStyle={{
@@ -34,10 +33,10 @@ class Blog extends Component {
                 </header>
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
+                
                 <Switch>
-                    <Route path="/" exact component={Posts} />
                     <Route path="/new-post" component={NewPost} />
-                    <Route path="/:id" exact component={FullPost} />
+                    <Route path="/posts" component={Posts} />
                 </Switch>
             </div>
         );
